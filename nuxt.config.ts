984836837d3a079228a028f48e6912037bdb4951
Rafaela Silva <79815197+rafasilva27@users.noreply.github.com>
2025-08-13 15:44:53 -0300

    feat: add title and favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss']
-})
+  modules: ["@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+      title: "Rick and Morty",
+      link: [{ rel: "icon", type: "image/png", href: "/FaviconA.png" }],
+    },
+  },
+});
